e0717e88d5ba18de2e7c1eaf4d36378e35d00c45
Om Barasara <ombarasara123@gmail.com>
2025-02-17 12:55:38 +0530

    ->update customer frame add next and previous button

--- src/components/mainpage/Customer.js
+++ src/components/mainpage/Customer.js
@@ -1,5 +1,11 @@
-import React, { useState } from 'react';
-import './Customer.css';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
+import "./Customer.css";
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"></link>
 
 const reviewsData = [
   {
@@ -29,17 +35,6 @@ const reviewsData = [
 ];
 
 const Customer = () => {
-  const [index, setIndex] = useState(0);
-  const reviewsPerSlide = window.innerWidth <= 480 ? 1 : window.innerWidth <= 768 ? 2 : 3;
-
-  const nextSlide = () => {
-    setIndex((prevIndex) => (prevIndex + reviewsPerSlide < reviewsData.length ? prevIndex + reviewsPerSlide : 0));
-  };
-
-  const prevSlide = () => {
-    setIndex((prevIndex) => (prevIndex - reviewsPerSlide >= 0 ? prevIndex - reviewsPerSlide : reviewsData.length - reviewsPerSlide));
-  };
-
   return (
     <div className="customer">
       <div className="customermain">
@@ -48,25 +43,48 @@ const Customer = () => {
         <p>Let’s Hear What Our Customers Have To Say</p>
       </div>
       <div className="customerframe">
-        <button className="prev" onClick={prevSlide}>&#10094;</button>
-        <div className="reviews-container">
-          {reviewsData.slice(index, index + reviewsPerSlide).map((review, i) => (
-            <div key={i} className="review-slide">
-              <div className="slide">
-                <p>{review.text}</p>
+        <Swiper
+          cssMode={true}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          }}
+          pagination={{ clickable: true }}
+          mousewheel={true}
+          keyboard={true}
+          modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+          className="mySwiper"
+          slidesPerView={1}
+          breakpoints={{
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+          }}
+        >
+          {reviewsData.map((review, index) => (
+            <SwiperSlide key={index}>
+              <div className="review-slide">
+                <div className="slide">
+                  <p>{review.text}</p>
+                </div>
+                <div className="user">
+                  <h3>{review.author}</h3>
+                </div>
+                <div className="stars">
+                  {[...Array(5)].map((_, i) => (
+                    <img
+                      key={i}
+                      src="image/singlestar.svg"
+                      alt="star"
+                      className="star-img"
+                    />
+                  ))}
+                </div>
               </div>
-              <div className="user">
-                <h3>{review.author}</h3>
-              </div>
-              <div className="stars">
-                {[...Array(5)].map((_, i) => (
-                  <img key={i} src="image/singlestar.svg" alt="star" className="star-img" />
-                ))}
-              </div>
-            </div>
+            </SwiperSlide>
           ))}
-        </div>
-        <button className="next" onClick={nextSlide}>&#10095;</button>
+          <div className="swiper-button-next"></div>
+          <div className="swiper-button-prev"></div>
+        </Swiper>
       </div>
     </div>
   );
