eaca621a468799bf7fb43b4e756f13a01d08ad56
Om Barasara <ombarasara123@gmail.com>
2025-02-20 09:30:50 +0530

    -> change in explore product and seller css

--- Frontend/src/components/mainpage/Exploreproduct.jsx
+++ Frontend/src/components/mainpage/Exploreproduct.jsx
@@ -1,28 +1,68 @@
-import React, { Component } from "react";
-import './Exploreproduct.css'
+import React from "react";
+import "./bestseller.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
 
-const ExploreProduct = () => {
-    return (
-        <div className="exploreproduct">
-            <div className="exploreproductheading">
-                <h1>Explore Our Product</h1>
-                <div className='shop'>
-                    <p>SHOP ALL</p>
-                    <img src="image/rightarrownew.svg" alt="arrow" />
-                </div>
-            </div>
-            <div className="product">
-                {[1, 2, 3, 4, 5].map((num) => (
-                    <div key={num} className={`item${num}`}>
-                        <img src={`image/dogitem${num}.svg`} alt={`dogitem${num}`} />
-                        <h2>Product Title {num}</h2>
-                        <p>£19.99 GBP</p>
-                        <a>View Details</a>
-                    </div>
-                ))}
-            </div>
+const Seller = () => {
+  const products = [
+    { id: 1, image: "image/dogitem1.svg", title: "Dog Food", price: "£19.99 GBP" },
+    { id: 2, image: "image/dogitem2.svg", title: "Chew Toy", price: "£14.99 GBP" },
+    { id: 3, image: "image/dogitem3.svg", title: "Dog Collar", price: "£9.99 GBP" },
+    { id: 4, image: "image/dogitem4.svg", title: "Dog Bed", price: "£29.99 GBP" },
+    { id: 5, image: "image/dogitem5.svg", title: "Dog Shampoo", price: "£12.99 GBP" },
+    { id: 6, image: "image/catitem1.svg", title: "Cat Litter", price: "£10.99 GBP" },
+    { id: 7, image: "image/catitem2.svg", title: "Cat Toy", price: "£8.99 GBP" },
+    { id: 8, image: "image/catitem3.svg", title: "Cat Bed", price: "£24.99 GBP" },
+    { id: 9, image: "image/catitem4.svg", title: "Cat Scratcher", price: "£19.99 GBP" },
+    { id: 10, image: "image/fishitem1.svg", title: "Fish Tank", price: "£59.99 GBP" },
+    { id: 11, image: "image/fishitem2.svg", title: "Fish Food", price: "£4.99 GBP" },
+    { id: 12, image: "image/fishitem3.svg", title: "Aquarium Light", price: "£19.99 GBP" }
+  ];
+
+  return (
+    <div className="seller">
+      <div className="sellerheading">
+        <h1>Explore Our Products</h1>
+        <div className="shop">
+          <p>SHOP ALL</p>
+          <img src="image/rightarrownew.svg" alt="arrow" />
         </div>
-    )
-}
+      </div>
 
-export default ExploreProduct;
+      <Swiper
+        cssMode={true}
+        navigation
+        pagination={{ clickable: true }}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        slidesPerView="auto"
+        spaceBetween={20}
+        breakpoints={{
+          480: { slidesPerView: 2, spaceBetween: 20 },
+          640: { slidesPerView: 2, spaceBetween: 20 },
+          768: { slidesPerView: 2, spaceBetween: 20 },
+          820: { slidesPerView: 3, spaceBetween: 20 },
+          1024: { slidesPerView: 3, spaceBetween: 20 },
+          1280: { slidesPerView: 4, spaceBetween: 20 },
+        }}
+      >
+        {products.map((item) => (
+          <SwiperSlide key={item.id}>
+            <div className="product-item">
+              <img src={item.image} alt={item.title} />
+              <h2>{item.title}</h2>
+              <p>{item.price}</p>
+              <a href="#">View Details</a>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Seller;
--- Frontend/src/components/mainpage/bestseller.jsx
+++ Frontend/src/components/mainpage/bestseller.jsx
@@ -7,6 +7,117 @@ import "swiper/css/pagination";
 import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
 
 const Seller = () => {
+  const categories = [
+    {
+      title: "For Dogs",
+      className: "fordog",
+      items: [
+        {
+          id: 1,
+          image: "image/dogitem1.svg",
+          title: "Dog Food",
+          price: "\u00a319.99 GBP",
+        },
+        {
+          id: 2,
+          image: "image/dogitem2.svg",
+          title: "Chew Toy",
+          price: "\u00a314.99 GBP",
+        },
+        {
+          id: 3,
+          image: "image/dogitem3.svg",
+          title: "Dog Collar",
+          price: "\u00a39.99 GBP",
+        },
+        {
+          id: 4,
+          image: "image/dogitem4.svg",
+          title: "Dog Bed",
+          price: "\u00a329.99 GBP",
+        },
+        {
+          id: 5,
+          image: "image/dogitem5.svg",
+          title: "Dog Shampoo",
+          price: "\u00a312.99 GBP",
+        },
+      ],
+    },
+    {
+      title: "For Cats",
+      className: "forcat",
+      items: [
+        {
+          id: 1,
+          image: "image/catitem1.svg",
+          title: "Cat Litter",
+          price: "\u00a310.99 GBP",
+        },
+        {
+          id: 2,
+          image: "image/catitem2.svg",
+          title: "Cat Toy",
+          price: "\u00a38.99 GBP",
+        },
+        {
+          id: 3,
+          image: "image/catitem3.svg",
+          title: "Cat Bed",
+          price: "\u00a324.99 GBP",
+        },
+        {
+          id: 4,
+          image: "image/catitem4.svg",
+          title: "Cat Scratcher",
+          price: "\u00a319.99 GBP",
+        },
+        {
+          id: 5,
+          image: "image/catitem5.svg",
+          title: "Cat Treats",
+          price: "\u00a35.99 GBP",
+        },
+      ],
+    },
+    {
+      title: "For Fish",
+      className: "forfish",
+      items: [
+        {
+          id: 1,
+          image: "image/fishitem1.svg",
+          title: "Fish Tank",
+          price: "\u00a359.99 GBP",
+        },
+        {
+          id: 2,
+          image: "image/fishitem2.svg",
+          title: "Fish Food",
+          price: "\u00a34.99 GBP",
+        },
+        {
+          id: 3,
+          image: "image/fishitem3.svg",
+          title: "Aquarium Light",
+          price: "\u00a319.99 GBP",
+        },
+        {
+          id: 4,
+          image: "image/fishitem4.svg",
+          title: "Water Filter",
+          price: "\u00a329.99 GBP",
+        },
+        {
+          id: 5,
+          image: "image/fishitem5.svg",
+          title: "Fish Net",
+          price: "\u00a36.99 GBP",
+        },
+      ],
+    },
+  ];
+
   return (
     <div className="seller">
       <div className="sellerheading">
@@ -17,73 +128,40 @@ const Seller = () => {
         </div>
       </div>
 
-      {/* For Dogs Section with Pagination & Navigation */}
-      <div className="fordog">
-        <h1>For Dogs</h1>
-        <Swiper
-          cssMode={true}
-          navigation={{
-            nextEl: ".dog-swiper-next",
-            prevEl: ".dog-swiper-prev",
-          }}
-          pagination={{ clickable: true }}
-          mousewheel={true}
-          keyboard={true}
-          modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-          className="mySwiper"
-          slidesPerView={1}
-          spaceBetween={20}
-          breakpoints={{
-            768: { slidesPerView: 2 },
-            1024: { slidesPerView: 3 },
-          }}
-        >
-          {[1, 2, 3, 4, 5].map((num) => (
-            <SwiperSlide key={num}>
-              <div className="dogitem">
-                <img src={`image/dogitem${num}.svg`} alt={`dogitem${num}`} />
-                <h2>Product Title {num}</h2>
-                <p>£19.99 GBP</p>
-                <a href="#">View Details</a>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-
-        {/* Custom Navigation Buttons */}
-        <div className="dog-swiper-prev swiper-button-prev"></div>
-        <div className="dog-swiper-next swiper-button-next"></div>
-      </div>
-
-      {/* For Cats Section */}
-      <div className="forcat">
-        <h1>For Cats</h1>
-        <div className="catitem">
-          {[1, 2, 3, 4, 5].map((num) => (
-            <div key={num} className={`catitem${num}`}>
-              <img src={`image/catitem${num}.svg`} alt={`catitem${num}`} />
-              <h2>Product Title {num}</h2>
-              <p>£19.99 GBP</p>
-              <a href="#">View Details</a>
-            </div>
-          ))}
+      {categories.map((category, index) => (
+        <div key={index} className={category.className}>
+          <h1>{category.title}</h1>
+          <Swiper
+            cssMode={true}
+            navigation
+            pagination={{ clickable: true }}
+            mousewheel={true}
+            keyboard={true}
+            modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+            slidesPerView="auto"
+            spaceBetween={20}
+            breakpoints={{
+              480: { slidesPerView: 2, spaceBetween: 20 },
+              640: { slidesPerView: 2, spaceBetween: 20 },
+              768: { slidesPerView: 2, spaceBetween: 20 },
+              820: { slidesPerView: 3, spaceBetween: 20 },
+              1024: { slidesPerView: 3, spaceBetween: 20 },
+              1280: { slidesPerView: 4, spaceBetween: 20 },
+            }}
+          >
+            {category.items.map((item) => (
+              <SwiperSlide key={item.id}>
+                <div className="product-item">
+                  <img src={item.image} alt={item.title} />
+                  <h2>{item.title}</h2>
+                  <p>{item.price}</p>
+                  <a href="#">View Details</a>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
-      </div>
-
-      {/* For Fish Section */}
-      <div className="forfish">
-        <h1>For Fish</h1>
-        <div className="fishitem">
-          {[1, 2, 3, 4, 5].map((num) => (
-            <div key={num} className={`fishitem${num}`}>
-              <img src={`image/fishitem${num}.svg`} alt={`fishitem${num}`} />
-              <h2>Product Title {num}</h2>
-              <p>£19.99 GBP</p>
-              <a href="#">View Details</a>
-            </div>
-          ))}
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
