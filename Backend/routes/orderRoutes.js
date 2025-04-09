a9e206dbc913dfd5e431e2861a03456036ab7e49
abkhunt <khuntabhi9925@gmail.com>
2025-04-09 14:40:43 +0530

    removed unn

--- Backend/routes/orderRoutes.js
+++ Backend/routes/orderRoutes.js
@@ -2,21 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { verifyToken } = require("../middleware/authuser");
 const {
-  getOrders,
-  createOrder,
-  updateOrderStatus,
-  getUserOrders,
-  getOrderById,
+  getOrders
 } = require("../controllers/orderController");
 
 router.get("/", verifyToken, getOrders);
 
-router.get("/user/:userId", verifyToken, getUserOrders);
-
-router.get("/:orderId", verifyToken, getOrderById);
-
-router.post("/", verifyToken, createOrder);
-
-router.patch("/:orderId/status", verifyToken, updateOrderStatus);
 
 module.exports = router;
